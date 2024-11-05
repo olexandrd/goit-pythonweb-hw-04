0039a37d3fb7310d5e451cc8915a76b94d7fc7a8
Olexandrd <olexandrd@gmail.com>
2024-11-05 22:05:56 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # goit-pythonweb-hw-04
+
+This script copy files from one directory to another and sort files by extension.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install dependencies.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+python sort.py --source_folder <source_dir> --output_folder <destination_dir>
+```
--- sort.py
+++ sort.py
@@ -1,22 +1,26 @@
+"""Program that copies and sorts files by their extension"""
+
 import asyncio
 import argparse
-import logging
 from collections import defaultdict
-import aiopath
+from aiopath import AsyncPath  # Ensure AsyncPath is used correctly
 import aioshutil
-import warnings
-
-logging.basicConfig(level=logging.ERROR)
 
 
-def ArgumentParser():
+def parse_arguments():
+    """Parse the command line arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--source_folder", type=str, help="Source folder path")
-    parser.add_argument("--output_folder", type=str, help="Output folder path")
+    parser.add_argument(
+        "--source_folder", type=str, help="Source folder path", required=True
+    )
+    parser.add_argument(
+        "--output_folder", type=str, help="Output folder path", required=True
+    )
     return parser.parse_args()
 
 
 async def read_folder(source_folder, files):
+    """Function checks if the path is a file or a folder"""
     async for path in source_folder.iterdir():
         if await path.is_file():
             files[path.suffix].append(path)
@@ -25,21 +29,27 @@ async def read_folder(source_folder, files):
 
 
 async def copy_file(file, output_folder):
+    """Function copies the file to the output folder"""
     await aioshutil.copy(file, output_folder / file.name)
 
 
 async def main():
-    args = ArgumentParser()
-    source_folder = aiopath.Path(args.source_folder)
-    output_folder = aiopath.Path(args.output_folder)
+    """Main function that sorts the files"""
+    args = parse_arguments()
+    source_folder = AsyncPath(args.source_folder)
+    output_folder = AsyncPath(args.output_folder)
     files = defaultdict(list)
+
     await read_folder(source_folder, files)
-    for ext, files in files.items():
+
+    tasks = []
+    for ext, ext_files in files.items():
         ext_folder = output_folder / ext
         await ext_folder.mkdir(parents=True, exist_ok=True)
-        await asyncio.gather(*[copy_file(file, ext_folder) for file in files])
+        tasks.extend([copy_file(file, ext_folder) for file in ext_files])
+
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-    warnings.filterwarnings("ignore", category=RuntimeWarning)
